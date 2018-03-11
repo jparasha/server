b228677263e2ab237fdbfba79beee4744c89d676
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-03-11 20:17:47 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+
+app.get('/', (req, res) =>{
+    console.log("called /");
+    res.send({"Hi" : "there!"});
+
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+    console.log("Server running on: " +PORT);
+});

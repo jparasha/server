becb002395c3f10c148418158b770ed67276bc1b
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-03-13 01:09:03 +0530

    Added google oauth

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-const express = require("express");
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./config/keys');
 const app = express();
 
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile);
+}));
 
-app.get('/', (req, res) =>{
-    console.log("called '/'");
-    res.send({"Hi" : "there!"});
-
+app.get('/test', (req, res) => {
+    console.log('test');
+    res.send({
+        'What': 'Yay!'
+    });
 });
 
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=>{
-    console.log("Server running on: " +PORT);
+app.listen(PORT, () => {
+    console.log("Server running on: " + PORT);
 });

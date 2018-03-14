dec73add00248b015828a4fa3fab75fa26c2a84b
jparasha <jayant.parashar@capgemini.com>
2018-03-14 19:12:04 +0530

    Added mongoose

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const moongoose = require('mongoose');
 const app = express();
+const keys = require('./config/keys');
 require('./services/passport');
 require('./routes/authRoutes')(app);
+
+moongoose.connect(keys.mongoURI);
+
+
+
+
+
+
 
 
 app.get('/test', (req, res) => {

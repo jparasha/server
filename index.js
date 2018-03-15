ab08b1c604cb85e9197c80bb2d1d435c2bac9411
jparasha <jayant.parashar@capgemini.com>
2018-03-15 14:20:49 +0530

    Added environment check

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('./services/passport');
 const app = express();
 app.use(
     cookieSession({
-        maxAge:30*24.60*60*1000,
+        maxAge:30*24*60*60*1000,
         keys:[keys.cookieKey]
     })
 );

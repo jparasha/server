f8e85d1e0e6f80d4e6ccdcfd20279255dbe89461
jparasha <jayant.parashar@capgemini.com>
2018-03-20 16:27:51 +0530

    Added logger to track

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -5,6 +5,7 @@ export default function (state = null, action) {
     switch (action.type) {
 
         case FETCH_USER:
+            console.log('CASE : FETCH_USER ', state);
             return action.payload || false;
         
         default:

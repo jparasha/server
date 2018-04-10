dc76857ab29879f9643ae824b96a64b693b1aedc
jparasha <jayant.parashar@capgemini.com>
2018-04-10 18:52:24 +0530

    Comment added

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
+export const fetchUser = () => async dispatch => {
+    const res = await axios.get('/api/current_user');
+    dispatch({ type: FETCH_USER, payload: res.data });
+};
 
 /* export const fetchUser = () => {
     return function() {
@@ -12,8 +16,3 @@ import { FETCH_USER } from './types';
     }
 };
  */
-export const fetchUser = () => async dispatch => {
-    const res = await axios.get('/api/current_user');
-    dispatch({ type: FETCH_USER, payload: res.data });
-};
-

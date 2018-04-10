e36207536546e62fc8d4155cb64bdf1668591555
jparasha <jayant.parashar@capgemini.com>
2018-04-10 18:54:42 +0530

    reducer debugging note added

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,8 +1,10 @@
+//authReducer
 import { FETCH_USER } from '../actions/types';
 export default function (state = null, action) {
 
     console.log(action);
     //action unable to be dispatched
+    //needs debugging
     
     switch (action.type) {
 

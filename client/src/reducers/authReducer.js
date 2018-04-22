8c01a60fb61a66f58e5e940eca36d26e536722cc
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-04-22 11:21:14 +0530

    logs added

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -5,12 +5,13 @@ export default function (state = null, action) {
     console.log(action);
     //action unable to be dispatched
     //needs debugging
-    
+    console.log('--fetchuserFrom authReducer--', FETCH_USER, action.type);
     switch (action.type) {
 
-        case FETCH_USER:
+        case FETCH_USER:{
             console.log('CASE : FETCH_USER ', state);
             return action.payload || false;
+        }
         
         default:
             return state;

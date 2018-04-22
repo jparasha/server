1a8037014f020bde881245ff117f9c08121a9209
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-04-22 11:21:37 +0530

    export modified

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 
-export default combineReducers ({
+/* export default combineReducers ({
+    
+}); */
+const rootReducer = combineReducers({
+    //weather : WeatherReducer
     auth : authReducer
-});
+  });
+  
+export default rootReducer;

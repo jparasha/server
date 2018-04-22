9b551d1e5a6b4fecc43570e545aa0c07b4f4b559
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-04-22 11:20:25 +0530

    dispatch removed

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,16 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
+console.log('fetchuserFrom Action', FETCH_USER);
 export const fetchUser = () => async dispatch => {
     const res = await axios.get('/api/current_user');
-    dispatch({ type: FETCH_USER, payload: res.data });
+    //dispatch({ type: FETCH_USER, payload: res.data });
+    var result= {
+        type: FETCH_USER,
+        payload : res.data
+    };
+    console.log(result);
+    return result;
 };
 
 /* export const fetchUser = () => {

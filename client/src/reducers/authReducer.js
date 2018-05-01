c14906bb4bfa203048422468c2417d31f0e2c71b
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-05-01 21:58:57 +0530

    removed logs

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -2,12 +2,12 @@
 import { FETCH_USER } from '../actions/types';
 export default function (state = null, action) {
 
-    console.log(action);
+    //console.log(action);
     //console.log('--fetchuserFrom authReducer--', FETCH_USER, action.type);
     switch (action.type) {
 
         case FETCH_USER:{
-            console.log('CASE : FETCH_USER ', state, action.payload);
+            //console.log('CASE : FETCH_USER ', state, action.payload);
             return action.payload || false;
         }
         

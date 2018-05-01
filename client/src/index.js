92bf317d93078202f3a71a8c28547223c0a34a13
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-05-01 18:46:21 +0530

    added axios helpers

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import 'materialize-css/dist/css/materialize.min.css';
+/* import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -13,4 +13,24 @@ var store = createStore(() => reducers, {}, applyMiddleware(reduxThunk));
 ReactDOM.render(
     <Provider store={store}><App /></Provider>,
     document.querySelector('#root')
+); */
+import 'materialize-css/dist/css/materialize.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
+import App from './components/App';
+import reducers from './reducers';
+
+// Development only axios helpers!
+import axios from 'axios';
+window.axios = axios;
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.querySelector('#root')
 );

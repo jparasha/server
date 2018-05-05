80834f18c10405ceeb6b068ec49e45ded263736b
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-05-06 03:26:44 +0530

    fixed build

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 
 if (process.env.NODE_ENV === "production"){
-    require('./client/build');
+    require('client/build');
 
     const path = require ('path');
     app.get('*', (req, res) =>{

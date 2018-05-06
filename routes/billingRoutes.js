37229f6ceacd92523d9ce06ab9b32b64b86f4a7d
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-05-06 15:02:54 +0530

    added charge

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -4,12 +4,13 @@ const stripe= require('stripe')(keys.stripeSecretKey);
 
 module.exports = app =>{
     app.post('/api/stripe/', async (req,res) =>{
-        console.log(req.body);
+        //console.log(req.body);
         const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
             description: '$5 for 5 credits',
             source: req.body.id
         });
+        console.log(charge);
     });
 };

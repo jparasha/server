ac31d490af56db8dc198f5181a9541927d93d25b
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-05-07 00:36:06 +0530

    changed to FeedUsBack

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
                     <Link
                         to={this.props.auth ? '/surveys' : '/'} 
                         className="left brand-logo">
-                        Emaily
+                        FeedUsBack
                     </Link>
                     <ul className="right">
                        {this.renderContent()}

0a3e066a14aa755e510471590f4b43204e6502e2
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-05-06 12:02:30 +0530

    added state

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Payments extends Component{
 
@@ -7,9 +9,9 @@ class Payments extends Component{
         return(
             <StripeCheckout
                 name={"Emaily"}
-                description={"$5 For 5 email Credits"}
+                description={"₹5 For 5 email Credits"}
                 amount={500}
-                token={token=> console.log(token)}
+                token={token=> this.props.handleToken(token)}
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
             >
                 <button className="btn">Add Credits</button>
@@ -17,4 +19,4 @@ class Payments extends Component{
         );
     }
 }
-export default Payments;
+export default connect (null, actions)(Payments);

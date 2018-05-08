55a8f98ccf401a63f6a0ee1d1a99297df7f05fb6
jparasha <jayant.parashar@capgemini.com>
2018-05-09 00:02:27 +0530

    updated index

--- client/src/index.js
+++ client/src/index.js
@@ -14,6 +14,7 @@ ReactDOM.render(
     <Provider store={store}><App /></Provider>,
     document.querySelector('#root')
 ); */
+
 import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';

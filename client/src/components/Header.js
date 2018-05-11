2fd3bb50133aaf331c9942a4e20c671cbd441d63
jparasha <jayant.parashar@capgemini.com>
2018-05-11 14:16:55 +0530

    Added user credits

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,7 +11,8 @@ class Header extends Component {
             case false: return (<li><a href='/auth/google'> Login With Google </a></li>)
             default: return [
                 <li key='1'> <Payments/> </li>,
-                <li key= '2'><a href='/api/logout'> Logout </a></li>
+                <li key='2' style={{ margin: '0 10px' }}>Credits: {this.props.auth.credits}</li>,
+                <li key='3'><a href='/api/logout'> Logout </a></li>
              ];
         }
     }

7cdc7b53b7f0bc5b52f84627a8a6fe3dab534c2a
jparasha <jayant.parashar@capgemini.com>
2018-05-15 13:47:10 +0530

    renamed stripe description

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -8,7 +8,7 @@ class Payments extends Component{
     render(){
         return(
             <StripeCheckout
-                name={"Emaily"}
+                name={"FeedUsBack"}
                 description={"₹5 For 5 email Credits"}
                 amount={500}
                 token={token=> this.props.handleToken(token)}

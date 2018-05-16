e4774f0b6daeb117a078be6bdc81a614147e080a
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-05-16 21:51:41 +0530

    removed comments

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,3 @@
-/* import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
-
-import App from './components/App';
-import reducers from './reducers';
-
-var store = createStore(() => reducers, {}, applyMiddleware(reduxThunk));
-//debug required 
-ReactDOM.render(
-    <Provider store={store}><App /></Provider>,
-    document.querySelector('#root')
-); 
-ERROR WAS: store is declared as var and not const
-
-*/
-
 import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';

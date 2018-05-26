c36b4ba0ca4803dec3b7df430602a2f845943571
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-05-26 15:33:51 +0530

    Added mail content

--- services/Mailer.js
+++ services/Mailer.js
@@ -3,7 +3,18 @@ const helper= sendgrid.mail;
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail{
-
+    constructor({ subject, recipients  }, content){
+        super();
+        this.from_email= new helper.Email('no-reply@feedusback.com');
+        this.subject= subject;
+        this.body= new helper.content('text/html', content);
+        this.recipients= this.formatAddresses(recipients);
+    }
+    formatAddresses(recipients){
+        return recipients.map(({email}) =>{
+            return new helper.Email(email);
+        });
+    }
 }
 
 module.exports = Mailer;

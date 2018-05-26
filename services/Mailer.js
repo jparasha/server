afd6296cd7187915749a74e8ca5139f0c38627a5
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-05-26 15:40:55 +0530

    added click tracking

--- services/Mailer.js
+++ services/Mailer.js
@@ -9,11 +9,18 @@ class Mailer extends helper.Mail{
         this.subject= subject;
         this.body= new helper.content('text/html', content);
         this.recipients= this.formatAddresses(recipients);
+
+        this.addContent(this.body);
+        this.addClickTracking();
     }
     formatAddresses(recipients){
         return recipients.map(({email}) =>{
             return new helper.Email(email);
         });
+    }
+    addClickTracking(){
+        const trackingSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTracking(true, true);
     }
 }
 

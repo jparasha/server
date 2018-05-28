d0c567a485ac6c3280dd94865a18748511b032e4
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-05-28 21:44:24 +0530

    Added html template

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,20 @@
 module.exports = () => {
-    return '<div>Hello There!</div>';
+    console.log('****');
+    return `
+        <html>
+            <body>
+                <div style = "text-align: center">
+                    <h3> I'd Like your input! </h3>
+                    <p> please answer the following Question: </p>
+                    <p>${survey.body}</p>
+                </div>
+                <div>
+                    <a href = "http://localhost:3000"> Yes </a>
+                </div>
+                <div>
+                    <a href = "http://localhost:3000"> No </a>
+                </div>
+            </body>
+        </html>
+    `;
 }

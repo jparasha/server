06426b54f3f3ff63628e229283097806f9cb0474
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-05-29 22:17:10 +0530

    added logger

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 export const fetchUser = () => async dispatch => {
+//const survey = {title : 'my title', subject : 'my sub', recipients :'officesync.jayant@gmail.com', body : 'its body' };
+//axios.post ('/api/surveys/', survey);
     const res = await axios.get('/api/current_user');
 
     dispatch({

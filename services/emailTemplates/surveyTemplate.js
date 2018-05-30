fd3a0d496787304f642a63b25bf71954940415b7
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-05-31 00:00:41 +0530

    fixed template formatting

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -7,14 +7,14 @@ module.exports = survey => {
                 <div style = "text-align: center">
                     <h3> I'd Like your input! </h3>
                     <p> please answer the following Question: </p>
-                    <p>${survey.body}</p>
-                </div>
-                <div>
-                    <a href = "http://localhost:3000"> Yes </a>
-                </div>
-                <div>
-                    <a href = "http://localhost:3000"> No </a>
-                </div>
+                    <p>${survey.body}</p>               
+                    <div>
+                        <a href = "http://localhost:3000"> Yes </a>
+                    </div>
+                    <div>
+                        <a href = "http://localhost:3000"> No </a>
+                    </div>
+                 </div>
             </body>
         </html>
     `;

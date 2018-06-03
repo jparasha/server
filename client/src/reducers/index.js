8341cfe1e7ed429b15475de0739014ce62e985e0
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-06-03 09:33:02 +0530

    added redux form reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,12 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
 
-/* export default combineReducers ({
-    
-}); */
-const rootReducer = combineReducers({
-    //weather : WeatherReducer
-    auth : authReducer
+ export default combineReducers ({
+
+    auth : authReducer,
+    form : reduxForm
   });
-  
-export default rootReducer;

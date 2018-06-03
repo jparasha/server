342e9e8e1245dd4f755b817d2f2cc98da3658f29
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-06-04 00:27:25 +0530

    wired up survey field

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
+
+class SurveyForm extends Component {
+    renderFields(){
+        return (
+             <div>
+                <Field type ="text" name = "title" Component = "input"/>
+            </div>
+        );
+    }
+    render(){
+        return(
+            <div>
+                    <form onSubmit = {this.props.handleSubmit(values => console.log(values) )}>
+                        {this.renderFields()}
+                        <button type = "submit" className = "btn">submit</button>
+
+                    </form>
+            </div>
+        );
+    }
+
+}
+export default reduxForm({
+    form : 'surveyForm'
+})(SurveyForm);

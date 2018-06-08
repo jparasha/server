0676108f651b54b962685f589a9e4e3591ff6b13
jparasha <jayant.parashar@capgemini.com>
2018-06-08 11:56:00 +0530

    added classname

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,9 +1,8 @@
 import React from 'react';
-
 export default ({input, label}) => {
     //console.log(props);
     return (
-        <div>
+        <div className="formFields">
             <label>{label}</label>
             <input {...input}/>
         </div>

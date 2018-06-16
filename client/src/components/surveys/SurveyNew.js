2a797ef02462cdce1a032df5a9fb3e512a435161
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-06-16 13:09:12 +0530

    added condition for form review

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
+import { realpathSync } from 'fs';
 
 
 class SurveyNew extends Component {
     
     state = { showFormReview : false };
+
+    renderContent(){
+        if (this.state.showFormReview){
+            return <SurveyFormReview/>;
+        }
+    
+         return <SurveyForm onSurveySubmit= {()=> this.setState({showFormReview : true})}/>;
+    }
     render(){
         return (
              <div>
-                <SurveyForm/>
+                {this.renderContent()}
             </div>
         )
     }

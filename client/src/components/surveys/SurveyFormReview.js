c5921079ab26be3c9eb0096d0d43c816e9e3abc6
jparasha <jayant.parashar@capgemini.com>
2018-06-20 14:39:11 +0530

    imported submit action

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,8 +2,9 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) =>{
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) =>{
     
     const reviewFields = _.map(formFields, ({name, label}) =>{
         return(
@@ -19,8 +20,12 @@ const SurveyFormReview = ({ onCancel, formValues }) =>{
         <div>
             <h4>Please confirm your entries</h4>
             {reviewFields}
-            <button className= "yellow darken-3 btn-flat" onClick= {onCancel}>
+            <button className= "yellow white-text darken-3 btn-flat" onClick= {onCancel}>
                 Back
+            </button>
+            <button className= "green white-text btn-flat right" onClick= {()=> submitSurvey(formValues)}>
+                Send Survey
+                <i className="material-icons right">email</i>
             </button>
         </div>
     );
@@ -31,4 +36,4 @@ function mapStateToProps(state){
         formValues: state.form.SurveyForm.values
     };
 }
-export default connect (mapStateToProps) (SurveyFormReview);
+export default connect (mapStateToProps, actions) (SurveyFormReview);

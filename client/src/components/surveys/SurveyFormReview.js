98dab99de747620f9357f3b02915c206308d4451
jparasha <jayant.parashar@capgemini.com>
2018-06-22 18:40:31 +0530

    added withRouter

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,9 +2,10 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) =>{
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) =>{
     
     const reviewFields = _.map(formFields, ({name, label}) =>{
         return(
@@ -23,7 +24,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) =>{
             <button className= "yellow white-text darken-3 btn-flat" onClick= {onCancel}>
                 Back
             </button>
-            <button className= "green white-text btn-flat right" onClick= {()=> submitSurvey(formValues)}>
+            <button className= "green white-text btn-flat right" onClick= {()=> submitSurvey(formValues, history)}>
                 Send Survey
                 <i className="material-icons right">email</i>
             </button>
@@ -36,4 +37,4 @@ function mapStateToProps(state){
         formValues: state.form.surveyForm.values
     };
 }
-export default connect (mapStateToProps, actions) (SurveyFormReview);
+export default connect (mapStateToProps, actions) (withRouter (SurveyFormReview));

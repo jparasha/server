129074695b75740bc14a595abb184fea94f5b791
MAIPL38-PC <jayant.parashar@capgemini.com>
2018-06-23 08:08:16 +0530

    fixed action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -12,14 +12,12 @@ export const fetchUser = () => async dispatch => {
     });
 };
 
-export const submitSurvey = (values, history)=>{
+export const submitSurvey = (values, history) => async dispatch => {
     const res = await axios.post('/api/surveys', values);
+  
     history.push('/surveys');
-    dispatch({
-        type: FETCH_USER, 
-        payload: res.data 
-    });
-};
+    dispatch({ type: FETCH_USER, payload: res.data });
+  };
 
 export const handleToken = token => async dispatch => {
     const res = await axios.post('/api/stripe', token);    

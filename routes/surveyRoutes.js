773d4ae1f03d691f82cd599fc0f3a21ecfd4635c
jparasha <jayant.parashar@capgemini.com>
2018-06-29 13:59:33 +0530

    removed unused comments

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -3,8 +3,8 @@ const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
-
 const Survey = mongoose.model('surveys');
+
 module.exports = app => {
     app.get('/api/surveys/thanks', (req, res) => {
         res.send('Thanks for feedback :)');
@@ -14,7 +14,6 @@ module.exports = app => {
         res.send({});
     })
     app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
-        console.log('////');
         const {
             title,
             subject,
@@ -41,9 +40,5 @@ module.exports = app => {
         } catch (err) {
             res.status(422).send(err);
         }
-
-
-
     });
-
 };

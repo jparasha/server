a53baf28eaf7f565bdc176acf82d0999b1b417df
jparasha <jayant.parashar@capgemini.com>
2018-07-04 18:38:33 +0530

    fixed spelling

--- middlewares/requireCredits.js
+++ middlewares/requireCredits.js
@@ -2,7 +2,7 @@
 module.exports =(req, res, next) =>{
 
     if(req.user.credits < 1) {
-        return res.status (403).send ({error: 'Not enought credits !'});
+        return res.status (403).send ({error: 'Not enough credits !'});
         const { title, subject, body, recipients } = req.body;
     }
     next ();

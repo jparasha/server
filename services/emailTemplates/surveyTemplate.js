69603adcc16b4ce9d0da07acbcb38383ec8c3d38
jparasha <jayant.parashar@capgemini.com>
2018-07-06 19:17:49 +0530

    Changed email Header

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,12 +1,10 @@
 const keys = require ('../../config/keys');
-module.exports = survey => {
-    console.log('****');
-    
+module.exports = survey => {    
     return `
         <html>
             <body>
                 <div style = "text-align: center">
-                    <h3> I'd Like your input! </h3>
+                    <h3> I'd Like to get your input! </h3>
                     <p> please answer the following Question: </p>
                     <p>${survey.body}</p>               
                     <div>
